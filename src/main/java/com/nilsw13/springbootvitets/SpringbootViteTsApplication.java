ade1cb3e61dfeed3e9335f03c29becec4b72ca56
nilsw13 <nils.wenting@laplateforme.io>
2024-07-12 21:47:04 +0200

    First commit

---
+++ src/main/java/com/nilsw13/springbootvitets/SpringbootViteTsApplication.java
@@ -0,0 +1,13 @@
+package com.nilsw13.springbootvitets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootViteTsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootViteTsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nilsw13/springbootvitets/SpringbootViteTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsw13.springbootvitets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootViteTsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
