c511742770b9a6e5a3bab14a72c69ee926d6f49e
nilsw13 <nils.wenting@laplateforme.io>
2024-11-20 13:07:49 +0100

    .

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Template to serve vite app to your springboot app into static files, i'm a beginner so if you have some tips to improve this template please let met know !
+Template to serve vite app to your springboot app into static files. This architecture is a first for me. so if you have some tips to improve this template please let met know !
 
 USAGE ---> RUN MVN CLEAN INSTALL SPRING-BOOT:RUN
+
+
+RUN BACKEND : SPRING-BOOT:RUN
+RUN FRONTEND : NPM RUN DEV
+
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-xl font-bold text-blue-600">
-          Test Tailwind
+      <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
+        <h1 className="mb-4 text-3xl font-bold md:mb-2 text-slate-300">Starter KIT</h1>
+        <h1 className="text-xl font-bold text-slate-300 md:text-2xl lg:text-3xl ">
+          <span className="text-green-600 md:ml-4 md:mr-4 ">Springboot</span> + <span className="text-yellow-300 md:ml-4 md:mr-4 ">React & Vite</span> + <span className="text-blue-400 md:ml-4 ">TailwindCSS</span>
         </h1>
+        <h2 className="mt-8 text-xl font-medium md:ml-4 text-slate-300"><a href="https://github.com/nilsw13">By nilsw13</a></h2>
       </div>
     )
   }
