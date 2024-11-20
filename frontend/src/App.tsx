31f1668845003e2f2c3dc94a80d0bd05d2e5e540
nilsw13 <nils.wenting@laplateforme.io>
2024-11-20 12:18:48 +0100

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
-import './App.css'
-
 function App() {
-
-  return (
-      <>
-          <div>
-              <p>hello from NilsW13 <br/> Enjoy !</p>
-
-          </div>
-      </>
-  )
-}
-
-export default App
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-xl font-bold text-blue-600">
+          Test Tailwind
+        </h1>
+      </div>
+    )
+  }
+  
+  export default App
